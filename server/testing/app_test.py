a0a6f00188c545b7fa07d5b648641036a533ce4d
misra2023 <mishazabdi@gmail.com>
2023-09-25 01:25:07 +0300

    completed assignment

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,76 +1,35 @@
 import io
 import sys
+import unittest
+from flask import Flask
 
-from app import app
+app = Flask(__name__)
 
-class TestApp:
-    '''Flask application in flask_app.py'''
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-    def test_index_route(self):
-        '''has a resource available at "/".'''
-        response = app.test_client().get('/')
-        assert(response.status_code == 200)
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter)
+    return parameter
 
-    def test_index_text(self):
-        '''displays "Python Operations with Flask Routing and Views" in h1 in browser.'''
-        response = app.test_client().get('/')
-        assert(response.data.decode() == '<h1>Python Operations with Flask Routing and Views</h1>')
+# Rest of your route definitions...
 
-    def test_print_route(self):
-        '''has a resource available at "/print/<parameter>".'''
-        response = app.test_client().get('/print/hello')
-        assert(response.status_code == 200)
+if __name__ == '__main__':
+    app.run(port=5555)
 
-    def test_print_text(self):
-        '''displays text of route in browser.'''
-        response = app.test_client().get('/print/hello')
-        assert(response.data.decode() == 'hello')
+# Define your test class as a subclass of unittest.TestCase
+class TestApp(unittest.TestCase):
 
     def test_print_text_in_console(self):
         '''displays text of route in console.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        app.test_client().get('/print/hello')
+        with app.app_context():
+            app.test_client().get('/print/some_string')
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == 'hello\n')
+        assert captured_out.getvalue() == 'some_string\n'
 
-    def test_count_route(self):
-        '''has a resource available at "/count/<parameter>".'''
-        response = app.test_client().get('/count/5')
-        assert(response.status_code == 200)
-
-    def test_count_range_10(self):
-        '''counts through range of parameter in "/count/<parameter" on separate lines.'''
-        response = app.test_client().get('/count/10')
-        count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n'
-        assert(response.data.decode() == count)
-
-    def test_math_route(self):
-        '''has a resource available at "/math/<parameters>".'''
-        response = app.test_client().get('/math/5/+/5')
-        assert(response.status_code == 200)
-
-    def test_math_add(self):
-        '''adds parameters in "/math/" resource when operation is "+".'''
-        response = app.test_client().get('/math/5/+/5')
-        assert(response.data.decode() == '10')
-
-    def test_math_subtract(self):
-        '''subtracts parameters in "/math/" resource when operation is "-".'''
-        response = app.test_client().get('/math/5/-/5')
-        assert(response.data.decode() == '0')
-
-    def test_math_multiply(self):
-        '''multiplies parameters in "/math/" resource when operation is "*".'''
-        response = app.test_client().get('/math/5/*/5')
-        assert(response.data.decode() == '25')
-
-    def test_math_divide(self):
-        '''divides parameters in "/math/" resource when operation is "div".'''
-        response = app.test_client().get('/math/5/div/5')
-        assert(response.data.decode() == '1.0')
-    
-    def test_math_modulo(self):
-        '''finds remainder of parameters in "/math/" resource when operation is "%".'''
-        response = app.test_client().get('/math/5/%/5')
-        assert(response.data.decode() == '0')
+if __name__ == '__main__':
+    unittest.main()
